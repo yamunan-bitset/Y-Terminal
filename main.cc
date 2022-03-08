92c95280ccb4dde3863bbcb041f053f49d8f8987
yamunan-bitset <yamunan.bitset@gmail.com>
2022-03-08 17:07:50 +0000

    Check for empty buffer

--- main.cc
+++ main.cc
@@ -3,26 +3,37 @@
 #include <iostream>
 #include <unistd.h>
 #include <pty.h>
+#include <cstring>
 
 extern int Spawn();
+
+bool empty(std::string_view str)
+{
+  if (str.size() == 0)
+    {
+      std::cout << "Empty" << std::endl;
+      return true;
+    }
+  std::cout << "Full" << std::endl;
+  return false;
+}
 
 int main(int argc, char *argv[])
 {
   QuickCG::screen(800, 800, 0, "Y-Terminal");
   int fd = Spawn();
-  char byte;
+  char buffer[4096];
   std::cout << fd << std::endl;
-  //write(fd, "J", 1);
   while (!QuickCG::done())
     {
-      ssize_t size = read(fd, &byte, 1);
-      if (byte != '\0')
+      ssize_t size = read(fd, buffer, 1);
+      std::cout << size << std::endl; // TODO: should not ouput -1
+      if (!empty(buffer))
 	{
-	  
-	  QuickCG::print(byte, 8, 8);
-	  std::cout << byte << std::endl;
+	  QuickCG::print(buffer, 8, 8);
+	  std::cout << "\033[0;31m" << buffer << "\033[0m" << std::endl;
 	}
-      byte = '\0';
+      std::memset(buffer, '\0', sizeof buffer);
       QuickCG::redraw();
     }
   return 0;
