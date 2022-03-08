bc57c7e7c7d1269355a9b988abeef29a8de4c298
yamunan-bitset <yamunan.bitset@gmail.com>
2022-03-08 16:30:47 +0000

    updated tty forking

--- main.cc
+++ main.cc
@@ -1,10 +1,29 @@
 #include "quickcg.h"
+#include "pty.h"
+#include <iostream>
+#include <unistd.h>
+#include <pty.h>
+
+extern int Spawn();
 
 int main(int argc, char *argv[])
 {
   QuickCG::screen(800, 800, 0, "Y-Terminal");
-  std::string someText;
-  QuickCG::getInputString(someText, "Enter some text:", false, 0, 0);
-  QuickCG::redraw();
-  QuickCG::sleep();
+  int fd = Spawn();
+  char byte;
+  std::cout << fd << std::endl;
+  //write(fd, "J", 1);
+  while (!QuickCG::done())
+    {
+      ssize_t size = read(fd, &byte, 1);
+      if (byte != '\0')
+	{
+	  
+	  QuickCG::print(byte, 8, 8);
+	  std::cout << byte << std::endl;
+	}
+      byte = '\0';
+      QuickCG::redraw();
+    }
+  return 0;
 }
--- tty.cc
+++ tty.cc
@@ -1,105 +1,27 @@
 #define _XOPEN_SOURCE 600
 #include <fcntl.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <pty.h>
+#include <cstdlib>
 
 #include "pty.h"
 
 #define TERM  "xterm-256color"
 #define SHELL getenv("SHELL")
 
-bool Pair(struct PTY* pty)
+int Spawn()
 {
-  char* slave_name;
-
-  pty->master = posix_openpt(O_RDWR | O_NOCTTY);
-  if (pty->master == -1)
+  int fd, pid;
+  struct winsize ws = {};
+  ws.ws_col = 800;
+  ws.ws_row = 800;
+  pid = forkpty(&fd, nullptr, nullptr, &ws);
+  if(!pid)
     {
-      perror("posix_openpt");
-      return false;
+      static char termstr[] = "TERM=xterm";
+      putenv(termstr);
+      execl(std::getenv("SHELL"), std::getenv("SHELL"), "-l", "-i", nullptr);
     }
-
-  if (grantpt(pty->master) == -1)
-    {
-      perror("grantpt");
-      return false;
-    }
-
-  if (unlockpt(pty->master) == -1)
-    {
-      perror("grantpt");
-      return false;
-    }
-
-  slave_name = ptsname(pty->master);
-  if (slave_name == NULL)
-    {
-      perror("ptsname");
-      return false;
-    }
-
-  pty->slave = open(slave_name, O_RDWR | O_NOCTTY);
-  if (pty->slave == -1)
-    {
-      perror("open(slave_name)");
-      return false;
-    }
-
-  return true;
+  fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
+  return fd;
 }
-
-bool Spawn(struct PTY* pty)
-{
-  pid_t p;
-  char* env[] = { "TERM="TERM, NULL };
-
-  p = fork();
-  if (p == 0)
-    {
-      close(pty->master);
-
-      setsid();
-      if (ioctl(pty->slave, TIOCSCTTY, NULL) == -1)
-        {
-	  perror("ioctl(TIOCSCTTY)");
-	  return false;
-        }
-
-      dup2(pty->slave, 0);
-      dup2(pty->slave, 1);
-      dup2(pty->slave, 2);
-      close(pty->slave);
-
-      execle(SHELL, SHELL, (char *)NULL, env);
-      return false;
-    }
-  else if (p > 0)
-    {
-      close(pty->slave);
-      return true;
-    }
-
-  perror("fork");
-  return false;
-}
-/*
-bool Resize(struct PTY* pty, struct X11* x11)
-{
-  struct winsize ws = {
-    .ws_col = x11->buf_w,
-    .ws_row = x11->buf_h,
-  };
-
-  
-  if (ioctl(pty->master, TIOCSWINSZ, &ws) == -1)
-    {
-      perror("ioctl(TIOCSWINSZ)");
-      return false;
-    }
-
-  return true;
-}
-*/
